4a5c312a14e9b280da89261f1f0da26fe7dc3fa7
Angela <angelachakupa.at@gmail.com>
2017-03-23 12:21:53 +0200

    EXAM GENERATING SYSTEM

--- buse/Questiongeneration/views.py
+++ buse/Questiongeneration/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def generate_question_paper(request):
+    return render(request, 'Questiongeneration/questiongenerator.html', {})
+
+def regenerate(request):
+    return render(request, '', {})
+
+def print_paper(request):
+    return render(request, '', {})
+    
+def reject_paper(request):
+    return render(request, '', {})
+
+def approve_paper(request):
+    return render(request, '', {})
+
+def save_paper(request):
+    return render(request, '', {})
